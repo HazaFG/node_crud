1a1ab667e9c1ff0a316b2b98ca2816d1e0d1a5a9
Haza Pro <hazaelfg@gmail.com>
2024-08-29 01:28:20 -0700

    registro de usuarios

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import morgan from 'morgan';
 
+import authRoutes from './routes/auth.routes.js'
+
 const app = express()
 
+//morgan muestra las peticiones que han llegado por consola
 app.use(morgan('dev'))
+app.use('/api', authRoutes)
 
 export default app;
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,8 @@
+//ESTO ESTA RELACIONADO CON EL AUTH.ROUTES.JS JAJA, vaya coshinero me siento haciendo
+
+//Van en formato json
+export const register = (req, res) => {
+    res.send('register')
+}
+
+export const login = (req, res) => {res.send('login')}
